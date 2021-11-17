bcdf80f832c78142b462c671f23783e400a55801
Matthieu Laqua <matthieu.laqua@kit.edu>
2021-11-17 12:31:15 +0100

    Add EntityResurrectEvent listener to detect when players use up totems

--- src/one/laqua/deathlogger/DeathLogger.java
+++ src/one/laqua/deathlogger/DeathLogger.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,5 +22,15 @@ public class DeathLogger extends JavaPlugin implements Listener {
         Location playerLoc = player.getLocation();
 
         System.out.println(player.getName() + " died: " + event.getDeathMessage() + " #XPlost=" + event.getDroppedExp() + " @[" + playerLoc.getBlockX() + "," + playerLoc.getBlockY() + "," + playerLoc.getBlockZ() + "]");
+    }
+
+    @EventHandler(ignoreCancelled = true) // cancelled == no totem equipped
+    public void onPlayerResurrectEvent(EntityResurrectEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+        Location playerLoc = player.getLocation();
+
+        System.out.println(player.getName() + " was resurrected: @[" + playerLoc.getBlockX() + "," + playerLoc.getBlockY() + "," + playerLoc.getBlockZ() + "]");
     }
 }
